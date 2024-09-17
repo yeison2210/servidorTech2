2b1d606c2f7ef6667aa51e5306d56dc24f3a9274
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-09-17 13:55:32 -0500

    Servidor

---
+++ src/main/java/com/example/servidor_suratech4/ServidorSuratech4Application.java
@@ -0,0 +1,13 @@
+package com.example.servidor_suratech4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorSuratech4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorSuratech4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/servidor_suratech4/ServidorSuratech4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servidor_suratech4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorSuratech4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
