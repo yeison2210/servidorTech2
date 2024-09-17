fb810157587de42fc7516bc2d21c0434bd48fe11
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-09-17 15:04:19 -0500

    server

---
+++ src/main/java/com/example/servidor_suratech4/Modelos/Paciente.java
@@ -0,0 +1,19 @@
+package com.example.servidor_suratech4.Modelos;
+
+import java.time.LocalDate;
+
+public class Paciente {
+    private Integer id;
+    private String nombres;
+    private Integer edad;
+    private LocalDate fechaNacimiento;
+    private String documento;
+    private String telefono;
+    private Integer copago;
+    private String email;
+    private String direccion;
+    private String ips;
+    private String medicoFamilia;
+
+}
+
